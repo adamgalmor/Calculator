87478754ec6aff25698a8edf6d4f2931dfa74150
adam_g <adam.galmor@playtech.com>
2016-06-28 15:01:27 +0300

    First solution

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -10,10 +10,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var ionic_angular_1 = require('ionic-angular');
+var moment = require('moment');
+// import {CalculatorModel} from './models/calculator.model.ts';
+// import {CalculatorService} from './services/calculator-service';
 var HomePage = (function () {
     function HomePage(navController) {
         this.navController = navController;
+        this.vecationStart = moment("2016/01/01");
+        this.vecationEnd = moment("2016/01/05");
     }
+    HomePage.prototype.calculate = function () {
+        var calculatedDate = moment(this.startDate).add(this.daysToAdd, 'days');
+        // alert('start date: ' + this.startDate);
+        // alert('daysToAdd: ' + this.daysToAdd);
+        // alert('calculatedDate: ' + moment(calculatedDate).format('DD/MM/YYYY'));
+        if (calculatedDate >= this.vecationStart && calculatedDate <= this.vecationEnd) {
+            this.result = this.vecationEnd.add(1, 'days').format('DD/MM/YYYY');
+        }
+        else {
+            this.result = calculatedDate.format('DD/MM/YYYY');
+        }
+    };
     HomePage = __decorate([
         core_1.Component({
             templateUrl: 'build/pages/home/home.html'
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,37 @@
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
+import * as moment from 'moment';
+// import {CalculatorModel} from './models/calculator.model.ts';
+// import {CalculatorService} from './services/calculator-service';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+
+  startDate: Date;
+  daysToAdd: number;
+  result: string
+  vecationStart = moment("2016/01/01");
+  vecationEnd = moment("2016/01/05");
+
   constructor(private navController: NavController) {
 
+  }
+
+  calculate() {
+    let calculatedDate = moment(this.startDate).add(this.daysToAdd,'days');
+
+    // alert('start date: ' + this.startDate);
+    // alert('daysToAdd: ' + this.daysToAdd);
+    // alert('calculatedDate: ' + moment(calculatedDate).format('DD/MM/YYYY'));
+
+
+    if (calculatedDate >= this.vecationStart && calculatedDate <= this.vecationEnd){
+      this.result = this.vecationEnd.add(1, 'days').format('DD/MM/YYYY');
+    }
+    else{
+      this.result = calculatedDate.format('DD/MM/YYYY');
+    }
   }
 }
